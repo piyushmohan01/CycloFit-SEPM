8226edcecaa17953fdcf50d132ab96bc8792b18b
piyushmohan01 <piyushmohan01@gmail.com>
2021-04-23 23:35:20 +0530

    File Structure Changed

--- cyclofit/__init__.py
+++ cyclofit/__init__.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask('__name__')
 app.config['SECRET_KEY'] = 'fcff7fee5b3c64fc4aaf6f3b45422af2'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///../database/dir.db'
 db = SQLAlchemy(app)
 
 from cyclofit import routes
