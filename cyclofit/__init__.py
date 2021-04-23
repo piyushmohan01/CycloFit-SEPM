18a8e05fb379d5409222e8ff23f724771ff4acfa
piyushmohan01 <piyushmohan01@gmail.com>
2021-04-24 02:40:42 +0530

    Fixed File-Structure

--- cyclofit/__init__.py
+++ cyclofit/__init__.py
@@ -1,9 +1,17 @@
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
-app = Flask('__name__')
+template_dir = os.path.abspath('../CycloFit/cyclofit/pages/templates')
+# print(template_dir)
+database_dir = os.path.abspath('../CycloFit/cyclofit/database')
+print(database_dir)
+# print(static_dir)
+static_dir = os.path.abspath('../CycloFit/cyclofit/pages/static')
+app = Flask('__name__', template_folder=template_dir, static_folder=static_dir)
 app.config['SECRET_KEY'] = 'fcff7fee5b3c64fc4aaf6f3b45422af2'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///../database/dir.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///../CycloFit/cyclofit/database/users.db'
+
 db = SQLAlchemy(app)
 
-from cyclofit import routes
+from cyclofit.pages import routes
