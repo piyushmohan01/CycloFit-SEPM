92f869b1ff4830ce2560c2f08c2bec97ef0eca8e
piyushmohan01 <piyushmohan01@gmail.com>
2021-04-24 21:05:08 +0530

    Support file added

--- cyclofit/__init__.py
+++ cyclofit/__init__.py
@@ -3,15 +3,11 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 template_dir = os.path.abspath('../CycloFit/cyclofit/pages/templates')
-# print(template_dir)
-database_dir = os.path.abspath('../CycloFit/cyclofit/database')
-print(database_dir)
-# print(static_dir)
 static_dir = os.path.abspath('../CycloFit/cyclofit/pages/static')
+
 app = Flask('__name__', template_folder=template_dir, static_folder=static_dir)
 app.config['SECRET_KEY'] = 'fcff7fee5b3c64fc4aaf6f3b45422af2'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///../CycloFit/cyclofit/database/users.db'
-
 db = SQLAlchemy(app)
 
 from cyclofit.pages import routes
