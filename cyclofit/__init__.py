844ccb6863bd89aa27ea50e01e38a8732982ca2f
piyushmohan01 <piyushmohan01@gmail.com>
2021-04-24 22:00:28 +0530

    User Authentication

--- cyclofit/__init__.py
+++ cyclofit/__init__.py
@@ -1,6 +1,8 @@
 import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 template_dir = os.path.abspath('../CycloFit/cyclofit/pages/templates')
 static_dir = os.path.abspath('../CycloFit/cyclofit/pages/static')
@@ -9,5 +11,7 @@ app = Flask('__name__', template_folder=template_dir, static_folder=static_dir)
 app.config['SECRET_KEY'] = 'fcff7fee5b3c64fc4aaf6f3b45422af2'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///../CycloFit/cyclofit/database/users.db'
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
 
 from cyclofit.pages import routes
--- cyclofit/pages/routes.py
+++ cyclofit/pages/routes.py
@@ -1,7 +1,8 @@
 from flask import render_template, url_for, flash, redirect
-from cyclofit import app
+from cyclofit import app, db, bcrypt
 from cyclofit.forms import RegistrationForm, ProfileForm, LoginForm
 from cyclofit.models import User, Profile
+from flask_login import login_user
 
 @app.route('/')
 @app.route('/welcome')
@@ -16,6 +17,12 @@ def home():
 def register():
     form = RegistrationForm()
     if form.validate_on_submit():
+        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        user = User(username=form.username.data, 
+                    email=form.email.data,
+                    password=hashed_password)
+        db.session.add(user)
+        db.session.commit()
         flash(f'Account Created for { form.username.data }', 'success')
         return redirect(url_for('register02'))
     return render_template('register.html', form=form)
@@ -24,15 +31,24 @@ def register():
 def register02():
     form = ProfileForm()
     if form.validate_on_submit():
-        return redirect(url_for('home'))
+        profile = Profile(area=form.area.data, 
+                          contact_no=form.contactno.data,
+                          dob=form.dob.data,
+                          emergency_no=form.emergencyno.data)
+        db.session.add(profile)
+        db.session.commit()
+        return redirect(url_for('login')) # home changed to login
     return render_template('register02.html', form=form)
 
+# checking login from database
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
     if form.validate_on_submit():
-        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
-            flash(f'You have been logged in!', 'success')
+        user = User.query.filter_by(email=form.email.data).first()
+        # if email exists and passwords match
+        if user and bcrypt.check_password_hash(user.password, form.password.data):
+            login_user(user)
             return redirect(url_for('home'))
         else:
             flash(f'Login Unsuccessful! Please check credentials!', 'danger')
