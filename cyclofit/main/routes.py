0f08671c8e22011729f0722e3bbcb4e2995db61d
piyushmohan01 <piyushmohan01@gmail.com>
2021-05-16 17:45:12 +0530

    Imports fixed and sorted

--- cyclofit/main/routes.py
+++ cyclofit/main/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template, request, Blueprint
+from flask import Blueprint, render_template, request
 
 main = Blueprint('main', __name__)
 
--- run.py
+++ run.py
@@ -1,8 +1,13 @@
+import socket
 from cyclofit import create_app
 
 app = create_app()
 
 if __name__ == '__main__':
+
+    hostname = socket.gethostname()
+    ip_address = socket.gethostbyname(hostname)
+
     app.run(debug=True)
     
     # , host='http://192.168.1.103:5000/') - EXT
