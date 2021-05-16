1cd5c9b3eee55710bcfeea6b6bf3f515bafe1985
piyushmohan01 <piyushmohan01@gmail.com>
2021-05-16 18:01:27 +0530

    Configuration Updated

--- cyclofit/config.py
+++ cyclofit/config.py
@@ -1,11 +1,14 @@
 import os
 
+from dotenv import load_dotenv
+load_dotenv()
+
 class Config:
-    SECRET_KEY = os.environ.get('DB_SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI')
+    SECRET_KEY = os.getenv('DB_SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
-    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
-    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    MAIL_USERNAME = os.getenv('MAIL_USERNAME')
+    MAIL_PASSWORD = os.getenv('MAIL_PASSWORD')
